59b32f9e2b80dd94cb236115e8822317d02bfc7f
Roseanna McFarlane <mcfarlrose@gmail.com>
2017-08-26 01:48:09 -0700

    added toy delete and toy edit methods

--- main.py
+++ main.py
@@ -28,7 +28,127 @@ APPLICATION_NAME = "Toystore Catalogue"
 
 @app.route('/')
 
+# @app.route('/login')
+# def showLogin():
+#     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+#                     for x in xrange(32))
+#     login_session['state'] = state
+#     return render_template('login.html', STATE=state)
 
+
+# @app.route('/gconnect', methods=['POST'])
+
+# def gconnect():
+#     # Validate state token
+#     if request.args.get('state') != login_session['state']:
+#         response = make_response(json.dumps('Invalid state parameter.'), 401)
+#         response.headers['Content-Type'] = 'application/json'
+#         return response
+#     # Obtain authorization code
+#     code = request.data
+
+#     try:
+#         # Upgrade the authorization code into a credentials object
+#         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+#         oauth_flow.redirect_uri = 'postmessage'
+#         credentials = oauth_flow.step2_exchange(code)
+#     except FlowExchangeError:
+#         response = make_response(
+#             json.dumps('Failed to upgrade the authorization code.'), 401)
+#         response.headers['Content-Type'] = 'application/json'
+#         return response
+
+#     # Check that the access token is valid.
+#     access_token = credentials.access_token
+#     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+#            % access_token)
+#     h = httplib2.Http()
+#     result = json.loads(h.request(url, 'GET')[1])
+#     # If there was an error in the access token info, abort.
+#     if result.get('error') is not None:
+#         response = make_response(json.dumps(result.get('error')), 500)
+#         response.headers['Content-Type'] = 'application/json'
+#         return response
+
+#     # Verify that the access token is used for the intended user.
+#     gplus_id = credentials.id_token['sub']
+#     if result['user_id'] != gplus_id:
+#         response = make_response(
+#             json.dumps("Token's user ID doesn't match given user ID."), 401)
+#         response.headers['Content-Type'] = 'application/json'
+#         return response
+
+#     # Verify that the access token is valid for this app.
+#     if result['issued_to'] != CLIENT_ID:
+#         response = make_response(
+#             json.dumps("Token's client ID does not match app's."), 401)
+#         print "Token's client ID does not match app's."
+#         response.headers['Content-Type'] = 'application/json'
+#         return response
+
+#     stored_access_token = login_session.get('access_token')
+#     stored_gplus_id = login_session.get('gplus_id')
+#     if stored_access_token is not None and gplus_id == stored_gplus_id:
+#         response = make_response(json.dumps('Current user is already connected.'),
+#                                  200)
+#         response.headers['Content-Type'] = 'application/json'
+#         return response
+
+#     # Store the access token in the session for later use.
+#     login_session['access_token'] = credentials.access_token
+#     login_session['gplus_id'] = gplus_id
+
+#     # Get user info
+#     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+#     params = {'access_token': credentials.access_token, 'alt': 'json'}
+#     answer = requests.get(userinfo_url, params=params)
+
+#     data = answer.json()
+
+#     login_session['username'] = data['name']
+#     login_session['picture'] = data['picture']
+#     login_session['email'] = data['email']
+
+#     output = ''
+#     output += '<h1>Welcome, '
+#     output += login_session['username']
+#     output += '!</h1>'
+#     output += '<img src="'
+#     output += login_session['picture']
+#     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+#     flash("you are now logged in as %s" % login_session['username'])
+#     print "done!"
+#     return output
+
+# @app.route('/gdisconnect')
+# def gdisconnect():
+#     access_token = login_session.get('access_token')
+#     if access_token is None:
+#         print 'Access Token is None'
+#         response = make_response(json.dumps('Current user not connected.'), 401)
+#         response.headers['Content-Type'] = 'application/json'
+#         return response
+#     print 'In gdisconnect access token is %s', access_token
+#     print 'User name is: '
+#     print login_session['username']
+#     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+#     h = httplib2.Http()
+#     result = h.request(url, 'GET')[0]
+#     print 'result is '
+#     print result
+#     if result['status'] == '200':
+#         del login_session['access_token']
+#         del login_session['gplus_id']
+#         del login_session['username']
+#         del login_session['email']
+#         del login_session['picture']
+#         response = make_response(json.dumps('Successfully disconnected.'), 200)
+#         response.headers['Content-Type'] = 'application/json'
+#         return response
+#     else:
+#         response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+#         response.headers['Content-Type'] = 'application/json'
+#         return response
 
 
 
@@ -53,8 +173,7 @@ def toystore(toystore_id):
 @app.route('/toys/<int:toystore_id>/<int:toy_id>/')
 def toyDescription(toystore_id, toy_id):
     toy = dbq.returnToy(toy_id)
-    return render_template('toy_info.html', toy=toy)
-
+    return render_template('toy_info.html', toy=toy, toystore_id=toystore_id)
 
 
 @app.route('/toystores/new/', methods=['GET', 'POST'])
@@ -71,6 +190,7 @@ def newToystore():
     else:
         return render_template('add_toystore.html')
 
+
 @app.route('/toystores/<int:toystore_id>/add-toy/', methods=['GET', 'POST'])
 def newToy(toystore_id):
     print toystore_id
@@ -82,12 +202,13 @@ def newToy(toystore_id):
                         toystore_id=toystore_id)
         session.add(addNewToy)
         session.commit()
-        flash("New toy added")
-        return redirect(url_for('toystores'))
+        #get new toy id
+        toy_id = addNewToy.id
+        # flash("New toy added")
+        return redirect(url_for('toyDescription', toystore_id=toystore_id, toy_id=toy_id))
     else:
         print toystore_id
         return render_template('add_toy.html', toystore_id=toystore_id)
-
 
 
 @app.route('/toystores/<int:toystore_id>/edit/', methods=['GET', 'POST'])
@@ -113,6 +234,29 @@ def editToystore(toystore_id):
         return render_template(
             'edit_toystore.html', toystore_id=toystore_id, item=editedStore)
 
+@app.route('/toys/<int:toystore_id>/<int:toy_id>/edit', methods=['GET', 'POST'])
+def editToy(toy_id,toystore_id):
+    editedToy = session.query(Toy).filter_by(id=toy_id).one()
+    print editedToy
+    if request.method == 'POST':
+        if request.form['name']:
+            editedToy.name = request.form['name']
+        if request.form['description']:
+            editedToy.description = request.form['description']
+
+        if request.form['color']:
+            editedToy.color = request.form['color']
+
+        if request.form['price']:
+            editedToy.price = request.form['price']
+
+        session.add(editedToy)
+        session.commit()
+        toy_id = editedToy.id
+        return redirect(url_for('toyDescription',toystore_id=toystore_id,toy_id=toy_id))
+    else:
+        return render_template('edit_toy.html', toystore_id=toystore_id,
+                               toy_id=toy_id, toy=editedToy)
 
 
 @app.route('/toystores/<int:toystore_id>/delete', methods=['GET', 'POST'])
@@ -127,6 +271,17 @@ def toystoreDelete(toystore_id):
     else:
         return render_template('delete_toystore.html', item=toystoredelete)
 
+@app.route('/toys/<int:toystore_id>/<int:toy_id>/delete', methods=['GET', 'POST'])
+def deleteToy(toy_id,toystore_id):
+    """Delete toystore function"""
+    #confim logged in? 
+    toy = session.query(Toy).filter_by(id=toy_id).one()
+    if request.method == 'POST':
+        session.delete(toy)
+        session.commit()
+        return redirect(url_for('toystore', toystore_id=toystore_id))
+    else:
+        return render_template('delete_toy.html', toy=toy, toystore_id=toystore_id )
 
 
 #JSON Endpoints
